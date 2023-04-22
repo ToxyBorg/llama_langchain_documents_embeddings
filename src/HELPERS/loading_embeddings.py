cd35cde917bd00468074373374a19a8a52992525
ToxyBorg <sehabiamir1@gmail.com>
2023-04-23 00:32:33 +0100

    done with the creating and saving the vectorstores

--- src/HELPERS/loading_embeddings.py
+++ src/HELPERS/loading_embeddings.py
@@ -1,6 +1,28 @@
-import pickle
+"""
+    This code defines a function called load_embeddings that loads embeddings from a file using the pickle module. 
+        
+    The function takes one argument: 
+        file_path which is the path to the file containing the embeddings. 
+    
+    The function: 
+        opens the file in binary mode, 
+        loads the embeddings using pickle.load(), and 
+        returns the embeddings.
+"""
 
+import pickle
 from langchain.embeddings.base import Embeddings
+
+
+"""
+    Loads embeddings from the specified file path using pickle.
+
+    Args:
+        file_path (str): Path to file containing embeddings.
+
+    Returns:
+        Embeddings: Loaded embeddings.
+"""
 
 
 def load_embeddings(file_path: str) -> Embeddings:
--- src/HELPERS/save_vectorstore.py
+++ src/HELPERS/save_vectorstore.py
@@ -1,17 +1,17 @@
 """
-This code defines a function called save_vectorstore that saves a FAISS index as a file at the specified directory path and file name. 
+    This code defines a function called save_vectorstore that saves a FAISS index as a file at the specified directory path and file name. 
 
-It imports the os module and the FAISS class from the langchain.vectorstores.faiss module. 
+    It imports the os module and the FAISS class from the langchain.vectorstores.faiss module. 
 
-The function takes three arguments: 
-    vectorstore which is the FAISS index to be saved, 
-    directory_path which is the path to the directory where the file will be saved, and 
-    file_name which is the name of the file to be saved. 
-    
-The function:
-    creates the directory if it doesn't exist, 
-    creates the file path, and 
-    saves the FAISS index to the file.
+    The function takes three arguments: 
+        vectorstore which is the FAISS index to be saved, 
+        directory_path which is the path to the directory where the file will be saved, and 
+        file_name which is the name of the file to be saved. 
+        
+    The function:
+        creates the directory if it doesn't exist, 
+        creates the file path, and 
+        saves the FAISS index to the file.
 """
 
 import os
